ffda03eb05d7c03cec268491676c639d02859a7a
Juan-Carlos-Pacheco <jp756@mail.fresnostate.edu>
2021-10-25 22:31:06 -0700

    commit

--- src/Form.js
+++ src/Form.js
@@ -1,12 +1,29 @@
-import React from 'react'
-import FormSignUp from './FormSignUp'
+import React, { useState } from 'react';
+import './Form.css';
+import FormSignup from './FormSignup';
+import FormVerified from './FormVerified';
 
 const Form = () => {
-    return (
-        <div>
-            <FormSignUp />
-        </div>
-    )
-}
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
-export default Form
+  function submitForm() {
+    setIsSubmitted(true);
+  }
+  return (
+    <>
+      <div className='form-container'>
+        <span className='close-btn'>×</span>
+        <div className='form-content-left'>
+          <img className='form-img' src='img/img-2.svg' alt='spaceship' />
+        </div>
+        {!isSubmitted ? (
+          <FormSignup submitForm={submitForm} />
+        ) : (
+          <FormVerified />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Form;
