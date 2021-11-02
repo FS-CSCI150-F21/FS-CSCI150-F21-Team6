bb8837aca7fdb0f6b91e2c85a5ec974a9ecd9440
Johnny Dean <johndevince@mail.fresnostate.edu>
2021-11-01 23:16:53 -0700

    Add basic functionality for timer

--- timerpage/src/App.js
+++ timerpage/src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState, useEffect} from 'react'
+const ProgressBar = ({pbSeconds, pbLength}) => {
+    const progressPercent = ((pbLength - pbSeconds)/ pbLength) * 100;
+    return (
+        <p>{progressPercent}%</p>
+    )
+}
 function App() {
+  const [ seconds, updateSeconds ] = useState(25*60)
+  const [ dungeonPercent, updateDP ] = useState(0)
+  const [ isRunning, updateIsRunning ] = useState(false)
+  const timeoutID = 0;
+  const testPomoLength = 25 * 60;
+    const secToMin = (seconds) => {
+        return `${Math.floor(seconds/60)}:${(seconds % 60 > 9) ? seconds % 60 : '0' + seconds % 60}`;
+    }
+
+    const startTimer = () => {
+        updateIsRunning(true);
+    }
+
+    useEffect(() => {
+        if(isRunning) {
+            console.log('wtf is going on')
+            const timeoutID = setTimeout(() => {
+                updateSeconds(seconds => seconds - 1)
+            }, 1000)
+        }
+
+        if (seconds === 1){
+            clearInterval(timeoutID);
+            updateIsRunning(false);
+            console.log('state change here')
+        }
+
+        clearInterval(timeoutID);
+    }, [isRunning, seconds]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <ProgressBar pbSeconds={seconds} pbLength={testPomoLength}/>
+        <div>{secToMin(seconds)}</div>
+        <button onClick={() => updateIsRunning(true)}>START</button>
+        <button onClick={() => updateIsRunning(false)}>STOP</button>
     </div>
+
   );
 }
 
