67d160f325195e78b410b44554de2ec6162a7bf1
Johnny Dean <johndevince@mail.fresnostate.edu>
2021-11-14 21:43:11 -0800

    Implement the start of being able to select and choose the active task for the quest list

--- src/components/QuestDisplay.js
+++ src/components/QuestDisplay.js
@@ -3,28 +3,15 @@ import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
 import axios from 'axios';
-
-const QDT = createTheme({
-
-})
-
-const Quest = ({ title, isActive }) => {
-    const handleClick = () => {
-        if (!isActive){
-
-        }
-    }
-
-    return (
-        <ThemeProvider theme={QDT}>
-            {/*className={ isActive === true ? classes.active : classes.base }*/}
-            <ListItemButton >{title}</ListItemButton>
-        </ThemeProvider>
-    )
-}
+// implement custom themes with createtheme and theme provider eventually
 
 const QuestDisplay = () => {
     const [quests, setQuests] = useState([])
+    const [selectedIndex, setSelectedIndex] = useState(1)
+
+    const handleClick = (id) => {
+
+    }
 
     useEffect(() => {
         axios.get('http://localhost:3001/data')
@@ -35,7 +22,9 @@ const QuestDisplay = () => {
 
     return (
         <List sx={{maxHeight: 100, overflow: 'auto', mt: 5}}>
-            {quests.map(quest => <Quest title={quest.title} isActive={quest.isActive}/>)}
+            {
+                quests.map(quest => <ListItemButton selected = { selectedIndex === quest.id } >{quest.title}</ListItemButton>)
+            }
         </List>
     )
 }
